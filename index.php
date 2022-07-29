d5bd6cbd2ecd2e9f7ce8aff5c396f00cf58e840b
Ichiroki <76273623+Ichiroki@users.noreply.github.com>
2022-07-29 19:34:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad neque consequatur quas placeat accusantium. Quidem eligendi minima molestias exercitationem dicta impedit numquam nam eveniet distinctio porro vel doloribus quas voluptatibus corrupti corporis ut voluptas hic, consequuntur aut asperiores iusto ab debitis veritatis eos. Doloremque eius molestias tenetur provident praesentium adipisci.</p>
+</body>
+
+</html>
